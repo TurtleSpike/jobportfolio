76b076988e095c18c4a4774f48a14456e8f18af2
Johan <ivan.sandra.0399@gmail.com>
2024-08-25 00:04:01 +0000

    1

---
+++ auto-backup.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+backupname=vpn-backup-`date '+%Y-%m-%d'`
+
+mkdir -p /backup && sudo chown -R $USER:$USER /backup
+cd /backup
+
+find /backup -type f -mtime +7 -delete
+sudo tar -zcvpf /backup/$backupname.tar.gz --exclude=/proc --exclude=/sys --exclude=/mnt --exclude=/media --exclude=/run --exclude=/dev --exclude=/backup --warning=no-file-changed /
+
+git add .
+git commit -m "$backupname"
+git push
